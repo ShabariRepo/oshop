ebd6e20e7d838e57e144f9dc05951d181a721fa7
shareDemCodez <shabari.cs@gmail.com>
2017-10-04 21:49:35 -0400

    add-login-logout-name-display-data-binding

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,23 @@
+import { AngularFireAuth } from 'angularfire2/auth';
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
+  user: firebase.User;
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+    // auth state is an observable that represents the state of the current user..
+    // the console log will display the state..
+    afAuth.authState.subscribe(user => this.user = user);
+   }
 
-  ngOnInit() {
+  logout(){
+    this.afAuth.auth.signOut();
   }
 
 }
