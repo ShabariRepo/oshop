cf6902c37b2a0ed3db745d8f4316b06fdbdd0e91
shareDemCodez <shabari.cs@gmail.com>
2017-10-04 21:59:00 -0400

    unsubscribing the observable for firebase and binding the var in the template

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,6 +1,7 @@
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'bs-navbar',
@@ -8,12 +9,14 @@ import * as firebase from 'firebase';
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent {
-  user: firebase.User;
+  // instead of the async task to unsubscribe for firebase you can also implement onDestroy in the constructor.. but this is easier
+  // Convention to name all the observable variables with $ so that it shows as an observable and can bind without having duplicate var names..
+  user$: Observable<firebase.User>;
 
   constructor(private afAuth: AngularFireAuth) {
     // auth state is an observable that represents the state of the current user..
-    // the console log will display the state..
-    afAuth.authState.subscribe(user => this.user = user);
+    // will automatically unsubscribe from the observable when that component is destroyed..
+    this.user$ = afAuth.authState;
    }
 
   logout(){
