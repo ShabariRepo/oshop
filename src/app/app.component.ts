d6eeceac63643acb4640cb623239e5a3d93275e9
shareDemCodez <shabari.cs@gmail.com>
2017-10-05 23:15:02 -0400

    BUG: fix redirect to home always - when refreshing page the local storage has the return URL set to '/' so always goes home on refresh - need to delete this return url from lcoal storage and redirec only the first time or if not logged in :rocket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,17 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private userService: UserService, private auth: AuthService, router: Router){
+  constructor(private userService: UserService, private auth: AuthService, router: Router) {
     // everytime a user logs in our out this observable (auth.user$) will emit a new value
     // In this case wont need to unsubscribe because its a single instance and when the user navigates away from the app, subscription is gone
     // But if you need to, then implement OnDestroy Interface..
     auth.user$.subscribe(user => {
-      if(user){
-        userService.save(user);
 
-        let returnUrl = localStorage.getItem('returnUrl');
-        router.navigateByUrl(returnUrl);
-      }
+      // if no user then return
+      // else save the user and redirect them
+      if (!user) return;
+      userService.save(user);
+
+      let returnUrl = localStorage.getItem('returnUrl');
+
+      if(!returnUrl) return;
+      
+      localStorage.removeItem('returnUrl');      
+      router.navigateByUrl(returnUrl);
     });
   }
 }
