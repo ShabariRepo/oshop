0d06a2f3cef527732879f2ad4fd1453944e22321
shareDemCodez <shabari.cs@gmail.com>
2017-10-06 00:07:56 -0400

    displaying the categories selection

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,3 +1,4 @@
+import { CategoryService } from './../category.service';
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent {
   products$;         // product observable
+  categories$;       // categories observable
 
-  constructor(productService: ProductService) {
-    // get all the products using the product service injection
+  constructor(productService: ProductService, categoryService: CategoryService) {
+    // get all the products and categories using the product service injection
     this.products$ = productService.getAll();
+    this.categories$ = categoryService.getAll();
    }
 }
