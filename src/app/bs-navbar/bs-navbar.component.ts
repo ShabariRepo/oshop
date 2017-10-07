193c95e314d6bb1fd457ca91f2a0e3d8e63d1d5b
shareDemCodez <shabari.cs@gmail.com>
2017-10-06 20:01:40 -0400

    display numbers of shopping cart items in navBar - Also implements the interfaces for shopping cart item and shopping cart

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,3 +1,5 @@
+import { Subscription } from 'rxjs/Subscription';
+import { ShoppingCartService } from './../shopping-cart.service';
 import { AppUser } from './../models/app-user';
 import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
@@ -7,16 +9,32 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent {
+export class BsNavbarComponent implements OnInit {
   appUser: AppUser;
+  shoppingCartItemCount: number;
 
   // Ahead of time compiler expects the fields we use in our template (html) as public
   // but now since we are not making it appear in the dom then make it private again
-  constructor(private auth: AuthService) { 
-    auth.appUser$.subscribe(appUser => this.appUser = appUser);
+  constructor(private auth: AuthService, private shoppingCartService: ShoppingCartService) { 
   }
 
    logout(){
      this.auth.logout();
+   }
+
+   // because shopping cart has async methods that are referenced here need to implement ngOnInit
+   async ngOnInit() {
+    this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
+
+    // get cart from shopping cart to display number
+    // returns an observable
+    // also don't need to unsubscribe since we have a single instance of the navBar Component in the dom through out the lifetime of the app
+    let cart$ = await this.shoppingCartService.getCart();
+    cart$.subscribe(cart => {
+      this.shoppingCartItemCount = 0;
+      for (let productId in cart.items)
+        this.shoppingCartItemCount += cart.items[productId].quantity;
+      
+    });
    }
 }
