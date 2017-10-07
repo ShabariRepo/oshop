64ea9b412c17a87c9fb3be85ccd74c9e8f90f653
shareDemCodez <shabari.cs@gmail.com>
2017-10-06 23:02:56 -0400

    display total price column on the shopping cart page

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -4,11 +4,19 @@ export class ShoppingCart {
   items: ShoppingCartItem[] = [];
 
   constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
-    for (let productId in itemsMap)
-      this.items.push(itemsMap[productId]);
+    for (let productId in itemsMap){
+      let item = itemsMap[productId];
+      this.items.push(new ShoppingCartItem(item.product, item.quantity));
+    }
   }
 
-
+  // gets the sum of prices in the shopping cart
+  get totalPrice() {
+    let sum = 0;
+    for(let productId in this.items)
+      sum+= this.items[productId].totalPrice;
+    return sum;
+  }
 
   // get total items from a shopping cart
   get totalItemsCount() {
